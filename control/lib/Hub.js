4703765c6f669ba21aa5c1710d137abc9af32165
Fredrik Román <roman.fredrik@gmail.com>
2015-06-22 18:42:59 +0200

    start and end state for each button

--- control/lib/Hub.js
+++ control/lib/Hub.js
@@ -4,26 +4,27 @@ const Hub = function (url) {
   const socket = io(url)
   socket.emit('i am a', 'controller')
 
+  const sendInput = (button, state) => () => socket.emit('input', { button, state })
+
   return {
     actions: {
-      onUp() {
-        socket.emit('input', 'up')
-      },
-      onDown() {
-        socket.emit('input', 'down')
-      },
-      onLeft() {
-        socket.emit('input', 'left')
-      },
-      onRight() {
-        socket.emit('input', 'right')
-      },
-      onA() {
-        socket.emit('input', 'a')
-      },
-      onB() {
-        socket.emit('input', 'b')
-      }
+      onUpStart: sendInput('up', 'start'),
+      onUpEnd:   sendInput('up', 'end'),
+
+      onDownStart: sendInput('down', 'start'),
+      onDownEnd: sendInput('down', 'end'),
+
+      onLeftStart: sendInput('left', 'start'),
+      onLeftEnd: sendInput('left', 'end'),
+
+      onRightStart: sendInput('right', 'start'),
+      onRightEnd: sendInput('right', 'end'),
+
+      onAStart: sendInput('a', 'start'),
+      onAEnd: sendInput('a', 'end'),
+
+      onBStart: sendInput('b', 'start'),
+      onBEnd: sendInput('b', 'end')
     }
   }
 }
--- control/lib/components/ClassicControl.js
+++ control/lib/components/ClassicControl.js
@@ -8,14 +8,14 @@ class ClassicControl extends React.Component {
       <div className="classic-control">
         <div className="classic-control-controls">
           <div className="classic-control-navigation">
-            <button onTouchStart={onUp}    className="classic-control-button classic-control-up">U</button>
-            <button onTouchStart={onDown}  className="classic-control-button classic-control-down">D</button>
-            <button onTouchStart={onLeft}  className="classic-control-button classic-control-left">L</button>
-            <button onTouchStart={onRight} className="classic-control-button classic-control-right">R</button>
+            <button onTouchStart={onUpStart}    onTouchEnd={onUpEnd} className="classic-control-button classic-control-up">U</button>
+            <button onTouchStart={onDownStart}  onTouchEnd={onDownEnd} className="classic-control-button classic-control-down">D</button>
+            <button onTouchStart={onLeftStart}  onTouchEnd={onLeftEnd} className="classic-control-button classic-control-left">L</button>
+            <button onTouchStart={onRightStart} onTouchEnd={onRightEnd} className="classic-control-button classic-control-right">R</button>
           </div>
           <div className="classic-control-actions">
-            <button onTouchStart={onA} className="classic-control-button classic-control-a">A</button>
-            <button onTouchStart={onB} className="classic-control-button classic-control-b">B</button>
+            <button onTouchStart={onAStart} onTouchEnd={onAEnd} className="classic-control-button classic-control-a">A</button>
+            <button onTouchStart={onBStart} onTouchEnd={onBEnd} className="classic-control-button classic-control-b">B</button>
           </div>
         </div>
       </div>
